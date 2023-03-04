5c22d1d0726591117b98526e6f844a8130f6a48d
justinwang <justinwang407@gmail.com>
2023-03-04 09:55:29 -0500

    Added button test functionality and more images

--- Oil-Exec-Simulator/Assets/Scripts/OilButton.cs
+++ Oil-Exec-Simulator/Assets/Scripts/OilButton.cs
@@ -6,8 +6,21 @@ using UnityEngine.UI;
 //OilButton functionality script
 public class OilButton : MonoBehaviour
 {
+    public double profits = 0;
+
+    public double moneyEarned = 80.0; 
+
+    public int counter = 0;
+
     //test function for testing onClick()
     public void Test(){
-        print("hello!");
+        counter++;
+        profits += moneyEarned;
+        print(profits);
+        if (counter > 10){
+            print("profits increased!");
+            moneyEarned += 10.0;
+            counter = 0;
+        }
     }
 }
