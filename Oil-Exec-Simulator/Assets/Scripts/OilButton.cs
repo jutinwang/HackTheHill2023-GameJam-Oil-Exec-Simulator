9698d3da1c32bc92cd3bbfb1fc412dd4b724be05
justinwang <justinwang407@gmail.com>
2023-03-04 09:15:43 -0500

    Added a script to the OilButton

---
+++ Oil-Exec-Simulator/Assets/Scripts/OilButton.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+//OilButton functionality script
+public class OilButton : MonoBehaviour
+{
+    //test function for testing onClick()
+    public void Test(){
+        print("hello!");
+    }
+}
