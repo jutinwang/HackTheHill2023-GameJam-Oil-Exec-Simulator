02190a26bda885163bb18b46ff62ea17e6367c92
Eli Wynn <eli.wynn7@gmail.com>
2023-03-05 02:26:58 -0500

    3 hours later

---
+++ Oil-Exec-Simulator/Assets/Scripts/ShopOpen.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopOpen : MonoBehaviour
+{
+    public GameObject palpatine;
+
+    public void onClick(){
+        palpatine.SetActive(true);
+    }
+    
+}
--- Oil-Exec-Simulator/Assets/Scripts/Shopping.cs
+++ Oil-Exec-Simulator/Assets/Scripts/Shopping.cs
@@ -3,36 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-[RequireComponent(typeof(Animation))]
-public class Shopping : MonoBehaviour
-{
- 
-    public AnimationClip animationClip;
- 
-    float speed = -1f;
-    new Animation animation;
- 
-    // Use this for initialization
-    void Start () {
-        this.animation = this.GetComponent<Animation>();
-        if(this.animationClip == null)
-        {
-            Debug.Log(this.GetType().ToString() + ": Animation Clip needed");
-            Destroy(this);
-        }
-    }
-   
- 
-    //MonoBehaviour Event. Need Collider to work correctly
-    void onClick()
-    {
-        if (this.animation.isPlaying) return; //exit
-        this.speed = -this.speed;
-        this.animation[this.animationClip.name].speed = this.speed;
-        this.animation.Play(this.animationClip.name);
-        if (this.speed < 0f) this.animation[this.animationClip.name].normalizedTime = 1f;
-    }
-}
-
+public class Shopping : MonoBehaviour{
     
+    public GameObject PopUp;
 
+    public void Start(){
+        PopUp.SetActive(false);
+    }
+
+    public void ByeByeStore(){
+        PopUp.SetActive(false);
+    }
+
+}
