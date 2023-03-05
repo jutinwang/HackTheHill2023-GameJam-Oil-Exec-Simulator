1041cc1396130502b260ae440ea8a4e24740d4e8
justinwang <justinwang407@gmail.com>
2023-03-05 00:47:37 -0500

    added functionality for buying

--- Oil-Exec-Simulator/Assets/Scripts/PopUpShop.cs
+++ Oil-Exec-Simulator/Assets/Scripts/PopUpShop.cs
@@ -1,16 +1,67 @@
-/*using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class PopUpShop : MonoBehaviour
 {
-    public GameObject popUpShop;
-    public Animator animator;
-    public TMP_Text popUpText;
-    
-    public void PopUp(String text){
-        //popUpShop.SetActive(true);
-        //popUpText.text = text;
-        //animator.SetTrigger("pop");
+    public static PopUpShop instance;
+
+    public int oilRigs = 0;
+    public double oilRigPrice;
+
+    public double oilFieldPrice;
+    public bool oilFieldBought = false;
+    public int oilFields = 0;
+
+    public double shallowRigPrice;
+    public bool shallowRigBought = false;
+    public int shallowRigs = 0;
+
+    public double deepSeaRigPrice;
+    public bool deepSeaRigBought = false;
+    public int deepSeaRigs = 0;
+
+    public double refineryPrice;
+    public double refineryMultiplier = 0.00;
+
+    private void Awake(){
+        instance = this;
     }
-}*/
+
+    public void buyOilDrill(){
+        if (OilButton.instance.currentFunds >= oilRigPrice){
+            OilButton.instance.buyItem(oilRigPrice);
+            oilRigs++;
+        }
+    }
+
+    public void buyOilField(){
+        if (OilButton.instance.currentFunds >= oilFieldPrice & oilFieldBought == false){
+            OilButton.instance.buyItem(oilFieldPrice);
+            oilFieldBought = true;
+        }
+    }
+
+    public void buyShallowRig(){
+        if (OilButton.instance.currentFunds >= shallowRigPrice & shallowRigBought == false){
+            OilButton.instance.buyItem(shallowRigPrice);
+            shallowRigBought = true;
+        }
+    }
+
+    public void buyDeepSeaRig(){
+        if (OilButton.instance.currentFunds >= deepSeaRigPrice & deepSeaRigBought == false){
+            OilButton.instance.buyItem(deepSeaRigPrice);
+            deepSeaRigBought = true;
+        }
+    }
+
+    public void buyRefinery(){
+        if (OilButton.instance.currentFunds >= refineryPrice){
+            OilButton.instance.buyItem(refineryPrice);
+            refineryMultiplier += 0.10;
+        }
+    }
+
+
+}
